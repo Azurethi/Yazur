c33ea708c0a2bf9c24b073a7698ede8d9f0a7af6
Azurethi <Github@Azurethi.me>
2021-04-14 03:02:34 +0100

    node init for testing

--- yazur.js
+++ yazur.js
@@ -1,6 +1,6 @@
 const Yazur_path = "./modules/interface";
 const Yazur = {
-    netmgr:   require(`${Yazur_path}/YololNetworkManager`),
+    netmgr:require(`${Yazur_path}/YololNetworkManager`),
     yChip: require(`${Yazur_path}/YololChip`),
     mChip: require(`${Yazur_path}/MemoryChip`),
     recvr: require(`${Yazur_path}/Reciever`)
